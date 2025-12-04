a47dfcbe24c1a77149724e5253bbc1dde372516c
Leonard <leonardfranz@MacBook-Pro-von-Leonard.local>
2025-12-04 17:25:56 +0100

    Add convex hull mesh

---
+++ scripts/run_record.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+uv run mjpython record_in_sim_demonstration.py --teleop.type=so101_leader \
+--teleop.port="/dev/tty.usbmodem5A460841371" \
+--teleop.id=lerobot_leader_arm \
+--teleop.use_degrees true \
+--teleop.calibration_dir /Users/leonardfranz/.cache/huggingface/lerobot/calibration/teleoperators/so101_leader/ 
