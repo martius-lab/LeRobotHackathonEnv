ae3b57f4df4ddf2e083e9458d701459f23d81ff1
Leonard Franz <franzleo@al-spiral.cs.uni-tuebingen.de>
2025-12-05 14:04:01 +0100

    Change RSI wrapper

--- src/lerobothackathonenv/wrappers.py
+++ src/lerobothackathonenv/wrappers.py
@@ -8,19 +8,32 @@ class ReferenceStateInitializationWrapper(gym.Wrapper):
     def __init__(self, env, trajectory_set: TrajectorySet):
         super().__init__(env)
         self.trajectory_set = trajectory_set
+        self.qpos_array = None
+        self.qvel_array = None
+        self._load_states()
+
+    def _load_states(self):
+        all_qpos = []
+        all_qvel = []
+        for traj_idx in range(self.trajectory_set.index):
+            trajectory_data = self.trajectory_set.get_trajectory(traj_idx)
+            simulator_states = trajectory_data['simulator_states']
+            for state in simulator_states:
+                all_qpos.append(state.qpos)
+                all_qvel.append(state.qvel)
+        self.qpos_array = np.array(all_qpos)
+        self.qvel_array = np.array(all_qvel)
 
     def reset(self, **kwargs):
         obs, info = self.env.reset(**kwargs)
-        self.trajectory_set.load_into_cache()
 
-        if self.trajectory_set.cache:
-            sampled_state = self.trajectory_set.sample_state(1)[0]
-
-            self.env.unwrapped.dm_control_env.physics.data.qpos[:] = sampled_state["qpos"]
-            self.env.unwrapped.dm_control_env.physics.data.qvel[:] = sampled_state["qvel"]
+        if len(self.qpos_array) > 0:
+            random_idx = np.random.randint(0, len(self.qpos_array))
+            self.env.unwrapped.dm_control_env.physics.data.qpos[:] = self.qpos_array[random_idx]
+            self.env.unwrapped.dm_control_env.physics.data.qvel[:] = self.qvel_array[random_idx]
             self.env.unwrapped.dm_control_env.physics.forward()
 
-        return sampled_state, info
+        return obs, info
 
     def step(self, action):
         return self.env.step(action)
